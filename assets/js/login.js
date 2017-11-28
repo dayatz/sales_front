2de07db03dee266c0943c92b263bc79326337ff1
dayatz <dayat.py@gmail.com>
2017-11-28 13:07:17 +0800

    add signup page

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,24 @@
 var vm = new Vue({
     el: '#login-form',
     delimiters: ["<%","%>"],
+    data: {
+        signup: {
+            username: '',
+            fullname: '',
+            email: '',
+            password: ''
+        }
+    },
     methods: {
         loginClick: function() {
             Materialize.toast('Login success! Redirecting...');
+            setTimeout(function() {
+                window.location.href = 'sales_list.html';
+            }, 1500);
+        },
+        signupForm: function() {
+            // send this.data.signup to server
+            Materialize.toast('Signup success! Redirecting...');
             setTimeout(function() {
                 window.location.href = 'sales_list.html';
             }, 1500);
